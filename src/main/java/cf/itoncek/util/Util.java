8a31b00373735ade40c0750b56df20a95412851b
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2022-01-09 12:33:57 +0100

    Code cleanup

--- src/main/java/cf/itoncek/util/Util.java
+++ src/main/java/cf/itoncek/util/Util.java
@@ -1,7 +1,6 @@
 package cf.itoncek.util;
 
 import com.sun.istack.Nullable;
-import org.apache.commons.logging.impl.Log4JLogger;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -11,32 +10,35 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Logger;
 
 public class Util {
-    public static String request (String url, @Nullable String content){
+    public static String request(String url, @Nullable String content) {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpResponse response = null;
+        HttpResponse response;
         try {
-            HttpPost request = new HttpPost("https://api.thingspeak.com/channels/1605396/bulk_update.json");
+            HttpPost request = new HttpPost(url);
             StringEntity params = new StringEntity(content);
             request.addHeader("content-type", "application/json");
             request.setEntity(params);
             response = httpClient.execute(request);
+            return response.toString();
         } catch (Exception ex) {
             ex.printStackTrace();
             return ex.toString();
-        } finally {
-            return response.toString();
         }
     }
 
-    public static String erqst(String url) throws IOException {
-        URL conn = new URL(url);
-        URLConnection urlcon = conn.openConnection();
-        BufferedReader out = new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
-        return out.readLine();
+    public static String erqst(String url) {
+        try {
+            URL conn = new URL(url);
+            URLConnection urlcon = conn.openConnection();
+            BufferedReader out = new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
+            return out.readLine();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 }
